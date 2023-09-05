b4c6dcaef8f6189270f28eb8fdc88159aed9530e
kareem <kareemesam52@yahoo.com>
2023-09-05 21:21:58 +0300

    update RemoteDataSource

--- remote/src/main/java/com/example/remote/dataSource/RemoteDataSourceImpl.kt
+++ remote/src/main/java/com/example/remote/dataSource/RemoteDataSourceImpl.kt
@@ -1,19 +1,22 @@
 package com.example.remote.dataSource
 
+import com.example.entity.Authentication.SignUpBody
+import com.example.remote.service.Authentication.AuthenticationService
 import com.example.remote.service.categories.CategoriesService
-import com.example.repository.dataSources.RemoteDataSource
+import com.example.repository.dataSources.remote.RemoteDataSource
+import com.example.repository.models.dto.Authentication.SignUpDto
 import com.example.repository.models.dto.brands.CategoriesDto
 import com.example.repository.models.dto.brands.SubCategoriesDto
-import com.example.repository.utils.NoInternetException
-import com.example.repository.utils.NullDataException
-import com.example.repository.utils.ServerException
-import retrofit2.Response
-import java.net.UnknownHostException
 import javax.inject.Inject
 
 class RemoteDataSourceImpl @Inject constructor(
     private val categoriesService: CategoriesService,
-) : RemoteDataSource {
+    private val authenticationService: AuthenticationService,
+) : RemoteDataSource, BaseRemoteDataSource() {
+    override suspend fun signUp(body: SignUpBody): SignUpDto {
+        return wrapApiCall { authenticationService.signUp(body) }
+    }
+
     override suspend fun getAllCategories(): CategoriesDto {
         return wrapApiCall { categoriesService.getAllCategories() }
     }
@@ -22,19 +25,5 @@ class RemoteDataSourceImpl @Inject constructor(
         return wrapApiCall { categoriesService.getAllSubCategories() }
     }
 
-    private suspend fun <T> wrapApiCall(request: suspend () -> Response<T>): T {
-        return try {
-            val response = request()
-            if (response.isSuccessful)
-                response.body() ?: throw NullDataException("empty data returned")
-            else {
-                throw when (response.code()) {
 
-                    else -> throw ServerException(response.message())
-                }
-            }
-        } catch (e: UnknownHostException) {
-            throw NoInternetException(e.message)
-        }
-    }
 }
--- repository/src/main/java/com/example/repository/dataSources/remote/RemoteDataSource.kt
+++ repository/src/main/java/com/example/repository/dataSources/remote/RemoteDataSource.kt
@@ -1,9 +1,16 @@
-package com.example.repository.dataSources
+package com.example.repository.dataSources.remote
 
+import com.example.entity.Authentication.SignUpBody
+import com.example.repository.models.dto.Authentication.SignUpDto
 import com.example.repository.models.dto.brands.CategoriesDto
 import com.example.repository.models.dto.brands.SubCategoriesDto
 
 interface RemoteDataSource {
+
+    //authentication
+    suspend fun signUp(body: SignUpBody):SignUpDto
+
+    //authentication
     // category
     suspend fun getAllCategories(): CategoriesDto
     suspend fun getAllSubCategories(categoryId: Int): SubCategoriesDto
