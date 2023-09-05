ae5fed0cd87e932c52ceeedf4167a829614130b2
kareem <kareemesam52@yahoo.com>
2023-09-05 21:35:17 +0300

    add user ApiKeyUSeCase

---
+++ useCase/src/main/java/com/example/usecase/UseCases/Authentiaction/UserApiKeyUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.usecase.UseCases.Authentiaction
+
+import com.example.usecase.repositoryInterfaces.LocalPrefsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UserApiKeyUseCase @Inject constructor(private val localPrefsRepository: LocalPrefsRepository) {
+
+    suspend fun setUserApiKey(key:String) = localPrefsRepository.setUserApiKey(key)
+    suspend fun getUserApiKey() : Flow<String> = localPrefsRepository.getUserApiKey()
+}
