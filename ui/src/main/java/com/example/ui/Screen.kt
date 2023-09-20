79aadd5f4f0495ce232d4179cd78cc6cff685d51
kareem <kareemesam52@yahoo.com>
2023-09-20 05:04:48 +0300

    update Screens

--- ui/src/main/java/com/example/ui/Screen.kt
+++ ui/src/main/java/com/example/ui/Screen.kt
@@ -1,7 +1,11 @@
 package com.example.ui
 
 sealed class Screen(val route:String){
-    object Home:Screen("HomeScreen")
-    object SignUp:Screen("SignUp")
-    object LogIn:Screen("LogIn")
+    data object Home:Screen("HomeScreen")
+    data object SignUp:Screen("SignUp")
+    data object LogIn:Screen("LogIn")
+    data object Search:Screen("SearchScreen")
+    data object Cart:Screen("CartScreen")
+    data object Favorite:Screen("FavoriteScreen")
+    data object Profile:Screen("ProfileScreen")
 }
