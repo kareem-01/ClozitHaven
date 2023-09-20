36d21dfb73dbb9f0edc8afe62914dc245b3d3e2e
kareem <kareemesam52@yahoo.com>
2023-09-20 05:14:27 +0300

    update ClozitHavenApp and NavGraph

--- ui/src/main/java/com/example/ui/ClozitHavenApp.kt
+++ ui/src/main/java/com/example/ui/ClozitHavenApp.kt
@@ -1,18 +1,39 @@
 package com.example.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.ui.BottomNav.BottomNavigation
+import com.example.ui.BottomNav.currentRoute
 import com.example.ui.nav.NavGraph
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 
 val LocalNavController = compositionLocalOf<NavHostController> { error("NO NavController") }
 
 @Composable
 fun ClozitHavenApp() {
     val navController = rememberNavController()
-    CompositionLocalProvider(LocalNavController provides navController) {
-        NavGraph(navController)
+    val screens = listOf(
+        Screen.Home.route
+    )
+
+
+    Scaffold(bottomBar = {
+        if (currentRoute(navController) in screens)
+            BottomNavigation(navController)
+    }) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            CompositionLocalProvider(LocalNavController provides navController) {
+                NavGraph(navController)
+            }
+        }
     }
+
+
 }
--- ui/src/main/java/com/example/ui/nav/ClozitHavenNavGraph.kt
+++ ui/src/main/java/com/example/ui/nav/ClozitHavenNavGraph.kt
@@ -4,15 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.ui.Screen
+import com.example.ui.screens.Search.search
 import com.example.ui.screens.home.homeRoute
 import com.example.ui.screens.logIn.logIn
 import com.example.ui.screens.signUp.SignUp
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController, startDestination = Screen.LogIn.route) {
+    NavHost(navController, startDestination = Screen.Home.route) {
         homeRoute()
         SignUp()
         logIn()
+        search()
     }
 }
