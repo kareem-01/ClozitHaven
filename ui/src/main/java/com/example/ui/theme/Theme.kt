7b6665b60281d927e629ac3bd102711ed6c096fd
kareem <kareemesam52@yahoo.com>
2023-09-20 05:04:30 +0300

    update Theme

--- ui/src/main/java/com/example/ui/theme/Theme.kt
+++ ui/src/main/java/com/example/ui/theme/Theme.kt
@@ -35,7 +35,8 @@ val lightCustomColor = CustomColors(
     textColor = lightTextColor,
     unFocusedTextField = lightUnFocusedTextField,
     hintColor = lightHintText,
-    background = lightBackground
+    background = lightBackground,
+    bottomNavColor = lightBottomNavColor,
 )
 
 private val ClozitHavenCustomColors = staticCompositionLocalOf { CustomColors() }
