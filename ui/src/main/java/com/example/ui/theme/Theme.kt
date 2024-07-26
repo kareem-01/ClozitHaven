82e57743163d55940158a666ffbe442e2d015cb2
kareem <kareemesam52@yahoo.com>
2024-07-26 09:28:18 +0300

    update theme & strings

--- ui/src/main/java/com/example/ui/theme/Theme.kt
+++ ui/src/main/java/com/example/ui/theme/Theme.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -41,12 +40,12 @@ val lightCustomColor = CustomColors(
     bottomNavColor = lightBottomNavColor,
     favoriteBackground = lightFavoriteBackground,
     onBackground87 = onLightBackground87,
-    )
+)
 
 private val ClozitHavenCustomColors = staticCompositionLocalOf { CustomColors() }
 
 @Composable
-fun ClozitHavenTheme(
+fun GlobaStyleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -55,7 +54,7 @@ fun ClozitHavenTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicLightColorScheme(context) else dynamicLightColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
